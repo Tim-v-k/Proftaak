b6684ee5d79b1047348df5f2ad8a18ed7d597472
Jarno Hendrikx <jarnohendrikx@outlook.com>
2020-05-27 13:23:36 +0200

    hi

--- index.php
+++ index.php
@@ -2,3 +2,15 @@
 
 ?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8" />
+	<meta name="description" content="Proftaak">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>GamingZone</title>
+</head>
+<body>
+
+</body>
+</html>
