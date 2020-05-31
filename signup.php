239bf21e817f193cf4b6b19c6e49127068273030
Siem Pouwels <siempouwels11@gmail.com>
2020-05-31 13:47:47 +0200

    sign up form

---
+++ signup.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<form>
+<label for="fname">First name:</label><br>
+<input type="text" id="fname" name="fname">
+  <br>
+<label for="lname">Last name:</label><br>
+<input type="text" id="userLastName" name="userLastName">
+<br>
+<label for="lname">Age:</label><br>
+<input type="text" id="userLastName" name="userLastName">
+</form>
