2793f32d07b27f064311f7b36e3d8629911eccb8
Tim van Kol <83349@roc-teraa.nl>
2020-06-15 11:45:41 +0200

    sorting

---
+++ submit.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php
+function processDrpdown($selectedVal) {
+    echo "Selected value in php ".$selectedVal;
+}        
+
+if ($_POST['dropdownValue']){
+    //call the function or execute the code
+    processDrpdown($_POST['dropdownValue']);
+}
+?>
+
+</body>
+</html>
