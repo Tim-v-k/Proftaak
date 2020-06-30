b032d92927fca00131b5e9e410f2951e45ede9b9
Jarno Hendrikx <jarnohendrikx@outlook.com>
2020-06-30 15:37:30 +0200

    hi

--- Contact.php
+++ Contact.php
@@ -10,27 +10,21 @@
 
         <?php require("inc\Bar.php");?>    
         <form id="frmContact" action="" method="post">
-                <div id="mail-status"></div>
-                <div class="contact-row column-right">
-        <label style="padding-top: 20px;">Name</label> <span
-            id="userName-info" class="info"></span><br /> <input
-            type="text" name="userName" id="userName"
-            class="demoInputBox">
+            <div id="mail-status"></div>
+            <div class="contact-row column-right">
+        <label style="padding-top: 20px;">Name</label> <span id="userName-info" class="info"></span><br /> <input type="text" name="userName" id="userName" class="demoInputBox">
     </div>
         <div class="contact-row column-right">
         <label>Email</label> <span id="userEmail-info" class="info"></span><br />
-        <input type="text" name="userEmail" id="userEmail"
-            class="demoInputBox">
+        <input type="text" name="userEmail" id="userEmail" class="demoInputBox">
     </div>
     <div class="contact-row">
         <label>Phone</label> <span id="subject-info" class="info"></span><br />
-        <input type="text" name="subject" id="subject"
-            class="demoInputBox">
+        <input type="text" name="subject" id="subject" class="demoInputBox">
     </div>
     <div>
         <label>Message</label> <span id="content-info" class="info"></span><br />
-        <textarea name="content" id="content" class="demoInputBox"
-            rows="3"></textarea>
+        <textarea name="content" id="content" class="demoInputBox" rows="3"></textarea>
     </div>
     <div>
         <input type="submit" value="Send" class="btnAction" />
@@ -40,8 +34,8 @@
             <img src="LoaderIcon.gif" />
     </div>
     <div id="contactus-text">
-            Contact us!
-</div>
+        Contact us!
+    </div>
 
         <?php require("inc\Footer.php");?>
 
